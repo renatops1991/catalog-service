b424bd827659022c65f23602b7b86a5dedceb440
Renato Pereira Santos <renatops1991@gmail.com>
2024-09-15 20:24:01 -0300

    test: ensure set true or false isActive property value if has false or true value
    Signed-off-by: Renato Pereira Santos <renatops1991@gmail.com>

--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -7,7 +7,6 @@ describe('Category', function() {
     beforeEach(function(){
         $this->category = new Category(
             name: 'foo',
-            id: 'foo',
             description: 'bar',
             isActive: true
         );
@@ -17,5 +16,26 @@ describe('Category', function() {
         expect($this->category->name)->toEqual('foo')
             ->and($this->category->description)->toEqual('bar')
             ->and($this->category->isActive)->toEqual(true);
+    });
+
+    describe("toggleActive", function(){
+        beforeEach(function(){
+            $this->category = new Category(
+                name: 'foo',
+                description: 'bar',
+                isActive: false
+            );
+        });
+
+        it('Should set true isActive property value if has false value', function() {
+            $this->category->toggleActive();
+            expect($this->category->isActive)->toBeTrue();
+        });
+
+        it('Should set false isActive property value if has true value', function() {
+            $this->category->isActive = true;
+            $this->category->toggleActive();
+            expect($this->category->isActive)->toBeFalse();
+        });
     });
 });
