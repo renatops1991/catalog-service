edb0d560306160197b7989a2d3fdce8582443223
Renato Pereira Santos <renatops1991@gmail.com>
2024-09-15 20:23:22 -0300

    refactor: add __set method to set value in property
    Signed-off-by: Renato Pereira Santos <renatops1991@gmail.com>

--- src/Application/Domain/Entity/Traits/MagicMethodsTrait.php
+++ src/Application/Domain/Entity/Traits/MagicMethodsTrait.php
@@ -5,7 +5,14 @@ namespace Application\Domain\Entity\Traits;
 
 trait MagicMethodsTrait {
     public function __get($property) {
-        if($this->$property) return $this->$property;
+        if(isset($this->$property)) return $this->$property;
+
+        $class = get_class($this);
+        throw new \Exception("Property {$property} does not exist in {$class}");
+    }
+
+    public function __set($property, $value) {
+        if(isset($this->$property)) return $this->$property = $value;
 
         $class = get_class($this);
         throw new \Exception("Property {$property} does not exist in {$class}");
