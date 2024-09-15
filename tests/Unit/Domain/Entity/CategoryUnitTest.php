3f3613b40399edd0172637ecbac1a7ede0bb50d7
Renato Pereira Santos <renatops1991@gmail.com>
2024-09-15 20:37:53 -0300

    test: ensure update name and description property
    Signed-off-by: Renato Pereira Santos <renatops1991@gmail.com>

--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -17,25 +17,41 @@ describe('Category', function() {
             ->and($this->category->description)->toEqual('bar')
             ->and($this->category->isActive)->toEqual(true);
     });
+});
 
-    describe("toggleActive", function(){
-        beforeEach(function(){
-            $this->category = new Category(
-                name: 'foo',
-                description: 'bar',
-                isActive: false
-            );
-        });
+describe("toggleActive", function(){
+    beforeEach(function(){
+        $this->category = new Category(
+            name: 'foo',
+            description: 'bar',
+            isActive: false
+        );
+    });
 
-        it('Should set true isActive property value if has false value', function() {
-            $this->category->toggleActive();
-            expect($this->category->isActive)->toBeTrue();
-        });
+    it('Should set true isActive property value if has false value', function() {
+        $this->category->toggleActive();
+        expect($this->category->isActive)->toBeTrue();
+    });
 
-        it('Should set false isActive property value if has true value', function() {
-            $this->category->isActive = true;
-            $this->category->toggleActive();
-            expect($this->category->isActive)->toBeFalse();
-        });
+    it('Should set false isActive property value if has true value', function() {
+        $this->category->isActive = true;
+        $this->category->toggleActive();
+        expect($this->category->isActive)->toBeFalse();
+    });
+});
+
+describe("update", function(){
+    beforeEach(function(){
+        $this->category = new Category(
+            name: 'foo',
+            description: 'bar',
+            isActive: false
+        );
+    });
+
+    it('Should update name and description property by Id', function() {
+        $this->category->update("john", "doe");
+        expect($this->category->name)->toEqual("john")
+            ->and($this->category->description)->toEqual("doe");
     });
 });
