ec29de400be30475372448475c54eb185bb618cb
Renato Pereira Santos <renatops1991@gmail.com>
2024-09-15 13:52:29 -0300

    refactor: use MagicsMethodTrait in Category class
    Signed-off-by: Renato Pereira Santos <renatops1991@gmail.com>

--- src/Application/Domain/Entity/Category.php
+++ src/Application/Domain/Entity/Category.php
@@ -2,12 +2,16 @@
 
 namespace Application\Domain\Entity;
 
+use Application\Domain\Entity\Traits\MagicsMethodTrait;
+
 class Category
 {
+    use MagicsMethodTrait;
+
     public function __construct(
-        public string $name,
-        public string $id ='',
-        public string $description = '',
-        public bool $isActive = true,
+        protected string $name,
+        protected string $id ='',
+        protected string $description = '',
+        protected bool $isActive = true,
     ) {}
 }
