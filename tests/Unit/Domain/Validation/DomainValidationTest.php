46a76ea3d13a61b1eb61c43f8d18dff3faf1bc28
Renato Pereira Santos <renatops1991@gmail.com>
2024-09-16 19:25:10 -0300

    test: ensure not throw exception if property provided is null
    Signed-off-by: Renato Pereira Santos <renatops1991@gmail.com>

--- tests/Unit/Domain/Validation/DomainValidationTest.php
+++ tests/Unit/Domain/Validation/DomainValidationTest.php
@@ -15,7 +15,19 @@ describe('Domain Validation', function () {
         $this->validation::isNotNull('');
     })->throws(EntityExceptionError::class, "Property cannot be null");
 
-    it("Should throw EntityExceptionError if property has character greater than value provided", function() {
-        $this->validation::validatePropertyRange('foo', 4, 5, "Property must be between 2 and 5");
+    it("Should throw EntityExceptionError if property has character greater than range value provided", function() {
+        $this->validation::validatePropertyRange('foo', 1, 2, "Property must be between 2 and 5");
     })->throws(EntityExceptionError::class, "Property must be between 2 and 5");
+
+    it("Should throw EntityExceptionError if property has character less than range value provided", function() {
+        $this->validation::validatePropertyRange('foo', 4, 10, "Property must be between 2 and 5");
+    })->throws(EntityExceptionError::class, "Property must be between 2 and 5");
+
+    it("Should throw EntityExceptionError if property is not null and quantity character less than range value provided", function() {
+        $this->validation::validateOptionalPropertyRange('f', 2, 5, "Property must be between 2 and 5");
+    })->throws(EntityExceptionError::class, "Property must be between 2 and 5");
+
+    it("Should not throw  EntityExceptionError if property provided is null", function() {
+        $this->validation::validateOptionalPropertyRange('', 2, 5, "Property must be between 2 and 5");
+    })->throwsNoExceptions();
 });
