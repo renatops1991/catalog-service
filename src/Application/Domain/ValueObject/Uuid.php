5c04e092b970aba8d436bde6afbfd1dc024696ef
Renato Pereira Santos <renatops1991@gmail.com>
2024-09-22 13:35:16 -0300

    test: ensure return correct uuid

--- src/Application/Domain/ValueObject/Uuid.php
+++ src/Application/Domain/ValueObject/Uuid.php
@@ -1,15 +1,23 @@
 <?php
 
 namespace Application\Domain\ValueObject;
-use http\Exception\InvalidArgumentException;
-use Ramsey\Uuid\Uuid as UuidGenerator;
 
+use \InvalidArgumentException;
+use Ramsey\Uuid\Uuid as UuidGenerator;
 
 class Uuid {
     public function __construct(
         protected string $id
     ) {
         $this->isValidId($id);
+    }
+
+    public static function generateId(): self {
+        return new self(UuidGenerator::uuid4()->toString());
+    }
+
+    public function __toString(): string {
+        return $this->id;
     }
 
     public function isValidId(string $id): void {
--- tests/Unit/Domain/ValueObject/UuidTest.php
+++ tests/Unit/Domain/ValueObject/UuidTest.php
@@ -1,18 +1,33 @@
 <?php
+namespace Tests\Unit\Domain\Entity\ValueObject;
 
 use Application\Domain\ValueObject\Uuid;
-use http\Exception\InvalidArgumentException;
+use \InvalidArgumentException;
 
 describe('Uuid', function () {
-    beforeEach(function () {
-        $this->uuid = new Uuid("12345678-1234-1234-1234-123456789012");
+
+    describe("isValid", function() {
+        beforeEach(function () {
+            $this->uuid = new Uuid("12345678-1234-1234-1234-123456789012");
+        });
+
+        it("Should validate id provided", function() {
+            $this->uuid->isValidId("12345678-1234-1234-1234-123456789012");
+        })->throwsNoExceptions();
+
+        it("Should throw InvalidArgumentException if id provided is not valid", function() {
+            $this->uuid->isValidId("abc");
+        })->throws(InvalidArgumentException::class, "Id provided is not valid");
     });
 
-    it("Should validate id provided", function() {
-        $this->uuid->isValidId("12345678-1234-1234-1234-123456789012");
-    })->throwsNoExceptions();
+    describe("generateId", function () {
+        beforeEach(function () {
+            $this->uuid = new Uuid("12345678-1234-1234-1234-123456789012");
+        });
 
-    it("Should throw InvalidArgumentException if id provided is not valid", function() {
-        $this->uuid->isValidId("abc");
-    })->throws(InvalidArgumentException::class, "Id provided is not valid");
+        it("Should generate a valid id", function(){
+            $response = $this->uuid::generateId();
+            expect($response)->toBeInstanceOf(Uuid::class);
+        });
+    });
 });
