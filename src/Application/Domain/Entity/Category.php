4c1bcca3c72eb2ff77eb1e3b5aa9f39ca88f1a75
Renato Pereira Santos <renatops1991@gmail.com>
2024-10-07 19:37:41 -0300

    feat: ensure add createdAt and updatedAt property in Category Entity

--- src/Application/Domain/Entity/Category.php
+++ src/Application/Domain/Entity/Category.php
@@ -6,6 +6,7 @@ use Application\Domain\Entity\Traits\MagicMethodsTrait;
 use Application\Domain\Exception\EntityExceptionError;
 use Application\Domain\Validation\DomainValidation;
 use Application\Domain\ValueObject\Uuid;
+use DateTime;
 
 class Category
 {
@@ -15,23 +16,30 @@ class Category
      * @throws EntityExceptionError
      */
     public function __construct(
-        protected Uuid | string $id = '',
-        protected string $name = '',
-        protected string $description = '',
-        protected bool $isActive = true,
-    ) {
+        protected Uuid|string     $id = '',
+        protected string          $name = '',
+        protected string          $description = '',
+        protected bool            $isActive = true,
+        protected DateTime|string $createdAt = '',
+        protected DateTime|string $updatedAt = '',
+    )
+    {
         $this->id = $this->id ? new Uuid($this->id) : Uuid::generateId();
+        $this->createdAt = $this->createdAt ? new DateTime($this->createdAt) : new DateTime();
+        $this->updatedAt = $this->updatedAt ? new DateTime($this->updatedAt) : new DateTime();
         $this->validate();
     }
 
-    public function toggleActive(): void {
+    public function toggleActive(): void
+    {
         $this->isActive = !$this->isActive;
     }
 
     /**
      * @throws EntityExceptionError
      */
-    public function update(string $name, string $description = ''): void {
+    public function update(string $name, string $description = ''): void
+    {
         $this->validate();
 
         $this->name = $name;
@@ -41,7 +49,8 @@ class Category
     /**
      * @throws EntityExceptionError
      */
-    public function validate(): void {
+    public function validate(): void
+    {
         DomainValidation::isNotNull($this->name, "This name not be empty");
         DomainValidation::validatePropertyRange($this->name, 2, 100, "Name must be greater than two characters and less than 100 characters");
         DomainValidation::validateOptionalPropertyRange($this->description, 2, 255, "Description must be greater than two characters and less than 255 characters");
