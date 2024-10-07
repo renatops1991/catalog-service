c686bf2ca3b58de2b08c01f5338f432fef4c8930
Renato Pereira Santos <renatops1991@gmail.com>
2024-10-07 19:37:48 -0300

    test: ensure add createdAt and updatedAt property in Category Entity

--- src/Application/Domain/Entity/Traits/MagicMethodsTrait.php
+++ src/Application/Domain/Entity/Traits/MagicMethodsTrait.php
@@ -3,22 +3,45 @@
 
 namespace Application\Domain\Entity\Traits;
 
-trait MagicMethodsTrait {
-    public function __get($property) {
-        if(isset($this->$property)) return $this->$property;
+trait MagicMethodsTrait
+{
+    /**
+     * @throws \Exception
+     */
+    public function __get($property)
+    {
+        if (isset($this->$property)) return $this->$property;
 
         $class = get_class($this);
         throw new \Exception("Property {$property} does not exist in {$class}");
     }
 
-    public function __set($property, $value) {
-        if(isset($this->$property)) return $this->$property = $value;
+    /**
+     * @param $property
+     * @param $value
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __set($property, $value)
+    {
+        if (isset($this->$property)) {
+            return $this->$property = $value;
+        }
 
         $class = get_class($this);
         throw new \Exception("Property {$property} does not exist in {$class}");
     }
 
-    public function getId(): string {
-        return (string) $this->id;
+    public function getId(): string
+    {
+        return (string)$this->id;
+    }
+
+    public function getCreatedAt(): String {
+        return $this->createdAt->format('Y-m-d H:i:s');
+    }
+
+    public function getUpdatedAt(): String {
+        return $this->createdAt->format('Y-m-d H:i:s');
     }
 }
--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace Tests\Unit\Domain\Entity;
 
 use Application\Domain\Entity\Category;
 use Application\Domain\Exception\EntityExceptionError;
 use Application\Domain\ValueObject\Uuid;
 
-describe('Category', function() {
-    beforeEach(function(){
+describe('Category', function () {
+    beforeEach(function () {
         $this->category = new Category(
             name: 'foo',
             description: 'bar',
@@ -14,16 +15,18 @@ describe('Category', function() {
         );
     });
 
-    it('Should exists properties in Category class', function(){
+    it('Should exists properties in Category class', function () {
         expect($this->category->name)->toEqual('foo')
             ->and($this->category->description)->toEqual('bar')
             ->and($this->category->isActive)->toEqual(true)
-            ->and($this->category->id)->toBeInstanceOf(Uuid::class);
+            ->and($this->category->id)->toBeInstanceOf(Uuid::class)
+            ->and($this->category->createdAt)->toBeInstanceOf(\DateTime::class)
+            ->and($this->category->updatedAt)->toBeInstanceOf(\DateTime::class);
     });
 });
 
-describe("toggleActive", function(){
-    beforeEach(function(){
+describe("toggleActive", function () {
+    beforeEach(function () {
         $this->category = new Category(
             name: 'foo',
             description: 'bar',
@@ -31,12 +34,12 @@ describe("toggleActive", function(){
         );
     });
 
-    it('Should set true isActive property value if has false value', function() {
+    it('Should set true isActive property value if has false value', function () {
         $this->category->toggleActive();
         expect($this->category->isActive)->toBeTrue();
     });
 
-    it('Should set false isActive property value if has true value', function() {
+    it('Should set false isActive property value if has true value', function () {
         $this->category->isActive = true;
         $this->category->toggleActive();
         expect($this->category->isActive)->toBeFalse();
@@ -44,23 +47,30 @@ describe("toggleActive", function(){
 });
 
 
-describe("update", function() {
-    it('Should update name and description property by Id', function() {
+describe("update", function () {
+    it('Should update name and description property by Id', function () {
         $uuid = Uuid::generateId();
         $this->category = new Category(
             id: $uuid,
             name: 'foo',
             description: 'bar',
-            isActive: false
+            isActive: false,
+            createdAt: '2023-01-01 00:00:00',
+            updatedAt: '2023-01-01 00:00:00'
         );
 
         $this->category->update("john", "doe");
+
         expect($this->category->name)->toEqual("john")
             ->and($this->category->description)->toEqual("doe")
-            ->and($this->category->getId())->toEqual($uuid);
+            ->and($this->category->getId())->toEqual($uuid)
+            ->and($this->category->createdAt)->toEqual(new \DateTime('2023-01-01 00:00:00'))
+            ->and($this->category->createdAt)->tobeInstanceOf(\DateTime::class)
+            ->and($this->category->updatedAt)->toBeInstanceOf(\DateTime::class)
+            ->and($this->category->updatedAt)->toEqual(new \DateTime('2023-01-01 00:00:00'));
     });
 
-    it('Should throw EntityExceptionError if description is less than two characters', function(){
+    it('Should throw EntityExceptionError if description is less than two characters', function () {
         $this->category = new Category(
             name: 'foo',
             description: 'b',
@@ -68,7 +78,7 @@ describe("update", function() {
         );
     })->throws(EntityExceptionError::class, "Description must be greater than two characters");
 
-    it('Should throw EntityExceptionError if name is less than two characters', function(){
+    it('Should throw EntityExceptionError if name is less than two characters', function () {
         $this->category = new Category(
             name: 'r',
             description: 'bar',
@@ -76,7 +86,7 @@ describe("update", function() {
         );
     })->throws(EntityExceptionError::class, "Name must be greater than two characters and less than 100 characters");
 
-    it('Should not throw EntityExceptionError if description or name has correct value', function(){
+    it('Should not throw EntityExceptionError if description or name has correct value', function () {
         $this->category = new Category(
             name: 'foo',
             description: 'john foo bar',
